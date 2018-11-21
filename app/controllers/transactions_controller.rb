6b1335275371ca8ba57de82957182ce9e9a25d98
Michael Totschnig <michael@totschnig.org>
2018-11-21 18:22:22 +0100

    find member and change balance

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,12 @@
 class TransactionsController < ApplicationController
   skip_before_action :verify_authenticity_token
-    def transactions
-    render plain: request.raw_post
+  
+  def transactions
+    parameters = params[:parameters]
+    member = Member.find_by name: parameters[:member]
+    amount = parameters[:amount]
+    member.balance = member.balance - amount
+    member.save
+    head :no_content
   end
 end
