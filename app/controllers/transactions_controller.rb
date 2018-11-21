7611708f30e1b2c228857b8126060f7dc57e9a01
Michael Totschnig <michael@totschnig.org>
2018-11-21 18:28:44 +0100

    params are directly part of params

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,9 +2,8 @@ class TransactionsController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def transactions
-    parameters = params[:parameters]
-    member = Member.find_by name: parameters[:member]
-    amount = parameters[:amount]
+    member = Member.find_by name: params[:member]
+    amount = params[:amount]
     member.balance = member.balance - amount
     member.save
     head :no_content
